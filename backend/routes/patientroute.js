fffd438dab5eea9ba8c24d552c8e9d6e523cb982
Achyuth S <165375463+achyuthsd@users.noreply.github.com>
2026-02-19 01:14:57 +0530

    Update patientroute.js

--- backend/routes/patientroute.js
+++ backend/routes/patientroute.js
@@ -2,7 +2,7 @@ import express from "express"
 import { postPatient,getPatient } from "../controllers/patientcontroller.js"
 
 const router = express.Router()
-
+//sdfghj
 router.post("/",postPatient)
 router.get("/:id",getPatient)
 
